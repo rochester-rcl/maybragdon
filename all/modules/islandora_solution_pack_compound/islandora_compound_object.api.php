9a8a52e241b53f9456c42354f69333f02d984daf
nate-rcl <nsarr@library.rochester.edu>
2015-12-21 15:35:21 -0500

    include islandora_solution_pack_compound module

---
+++ all/modules/islandora_solution_pack_compound/islandora_compound_object.api.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * API documentation.
+ */
+
+/**
+ * Notify other modules about the addition of objects to a compound.
+ *
+ * @param array $objects
+ *   An array of AbstractObjects added to the parents, to produce compounds.
+ * @param array $parent_pids
+ *   An array of PIDs representing the parents to which each object was added,
+ *   to make each member of $objects a member of multiple compounds.
+ */
+function hook_islandora_compound_object_children_added_to_parent($objects, $parent_pids) {
+
+}
+
+/**
+ * Notify other modules about the removal of objects from a compound.
+ *
+ * @param array $object
+ *   An array of AbstractObjects removed from the parents.
+ * @param array $parent_pids
+ *   An array of PIDs representing the parents to which each object was added,
+ *   to make each member of $objects a member of multiple compounds.
+ */
+function hook_islandora_compound_object_children_removed_from_parent($object, $parent_pids) {
+
+}
+
+/**
+ * Registry menu paths for the management controls for compound to appear on.
+ */
+function hook_islandora_compound_object_management_control_paths() {
+  return array(
+    'islandora/object/%/your/menu/path/here',
+  );
+}
